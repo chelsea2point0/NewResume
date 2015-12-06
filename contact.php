0cb2d5b7cedb98206aba942109d84eadd7c0b418
chelsea2point0 <chelseajoyschaefer@gmail.com>
2015-12-05 23:44:16 -0500

    added captcha verification to Connect page and edited relative links on index, portfolio, resume, and contact code to point to contact php page

--- contact.php
+++ contact.php
@@ -7,35 +7,16 @@
   	<link href="css/style.css" rel="stylesheet">
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<script src="scripts/jQuery.js"></script>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
 	<style>
-		footer {position: absolute;	bottom: 0;}
+		footer {position: absolute;	bottom: 0;};
+		@media screen and (max-width: 768px) {
+			footer {position: relative;}
+		}
 	</style>
 
 
 <?php
-# run from http://localhost:7777/PHP/contact.html
-
-	extract ($_REQUEST, EXTR_SKIP);
-
-	if (isset ($name) && isset($email) && isset($phone) 
-	&& isset($message)) {
-
-		/* Subject and Email Variables*/ 
-		$recipient = "chelsea2point0@gmail.com";
-		$msgSubject = "New Message from chelseaschaefer.com";
-		$headers = array('From: chelsea2point0@gmail.com',
-			'Reply To: chelsea2point0@gmail.com',
-			'X-Mailer: PHP/' . PHP_VERSION
-			);
-		$headers = implode("\r\n", $headers);
-		$msgBody = 
-			"You have received a new message from $name: \r\n
-			Phone: $phone \r\n 
-			Email: $email \r\n
-			Message: $message \r\n";
-		
-		$send = mail($recipient, $msgSubject, $msgBody, $headers, "-f chelsea2point0@gmail.com");		
-	};
 
 ?>
 
@@ -44,7 +25,7 @@
 <body>
 	<div class="container">
 		<header id="top">
-			<p><a href="resume.html">RESUME</a> | <a href="portfolio.html">PORTFOLIO</a> | <a href="contact.html" class="currentPage">CONNECT</a></p>
+			<p><a href="resume.html">RESUME</a> | <a href="portfolio.html">PORTFOLIO</a> | <a href="contact.php" class="currentPage">CONNECT</a></p>
 			<hr>
 		</header>
 		<section class="center">
@@ -65,38 +46,84 @@
 						<td class="right"><label for="name">Name:</label></td>
 						<td><input type="text" name="name" id="name" required></td>
 					</tr>
-
 					<tr>
 						<td class="right"><label for="email">E-mail:</label></td>
 						<td><input type="text" name="email" id="email" required></td>
 					</tr>
-
 					<tr>
 						<td class="right"><label for="phone">Phone:</label></td>
 						<td><input type="text" name="phone" id="phone" required></td>
 					</tr>
-
 					<tr>
 						<td class="right"><label for="message">Message to Chelsea:</label></td>
 						<td><textarea name="message" id="message" cols="50" rows="5" required></textarea></td>
 					</tr>
-
 					<tr>
 						<td></td>
 						<td><input type="submit" name="submit" value="Send Message"></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><br><div class="g-recaptcha" data-sitekey="6LdybBITAAAAAG9OTsksI7SU5v0a-_3gMyOCpNf3"></div></td>
 					</tr>
 				</table>
 			</form>
 
 		<?php
-		if (isset ($submit)) {
+		# run from http://localhost:7777/PHP/contact.html
 
-			if ($send) {
-				echo '<br><p style="color: red;">Thank you for your message! I will be in touch shortly.</p>';
-				} else {
-				echo '<br><p style="color: red;">Oops, something went wrong. Message did not send.</p>';
-			}
+	if(isset($_POST['submit']) && !empty($_POST['submit']) && !empty($_POST['name'])){
+			$name;$email;$phone;$message;$captcha;
+	        if(isset($_POST['name'])){
+	          $name=$_POST['name'];
+	        }if(isset($_POST['email'])){
+	          $email=$_POST['email'];
+	        }if(isset($_POST['phone'])){
+	          $phone=$_POST['phone'];
+	        }if(isset($_POST['message'])){
+	          $message=$_POST['message'];
+	        }
+
+  		if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){	
+			if(isset($_POST['g-recaptcha-response'])){
+	          $captcha=$_POST['g-recaptcha-response'];
+	        }
+			
+
+		        //your site secret key
+		    $secret = '6LdybBITAAAAADLR08gnrIGzqWrAIlsda0cn56R8';
+		    //get verify response data
+		    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+		    $responseData = json_decode($verifyResponse);
+		    if($responseData->success){
+		        //contact form submission code
+		        /* Subject and Email Variables*/ 
+				$recipient = "chelsea2point0@gmail.com";
+				$msgSubject = "New Message from chelseaschaefer.com";
+				$headers = array('From: chelsea2point0@gmail.com',
+					'Reply To: chelsea2point0@gmail.com',
+					'X-Mailer: PHP/' . PHP_VERSION
+					);
+				$headers = implode("\r\n", $headers);
+				$msgBody = 
+					"You have received a new message from $name: \r\n
+					Phone: $phone \r\n 
+					Email: $email \r\n
+					Message: $message \r\n";
+
+				$send = mail($recipient, $msgSubject, $msgBody, $headers, "-f chelsea2point0@gmail.com");	
+
+		        $succMsg = 'Your message has been submitted successfully!';
+		        echo '<br><p style="color: blue;">'. $succMsg . '</p>';
+		    }else{
+		        $errMsg = 'Robot verification failed, please try again.';
+		        echo '<br><p style="color: red;">'. $errMsg . '</p>';
+		    }
+		}else{
+		    $errMsg = 'Please click on the reCAPTCHA box.';
+		    echo '<br><p style="color: red;">'. $errMsg . '</p>';
 		}
+	}
 		?>	
 
 		</section>
